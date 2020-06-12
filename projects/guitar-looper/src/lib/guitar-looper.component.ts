5b879761cb3cdb1ba7b2d80477b2f96c34fd3e82
lironh <lironenk@gmail.com>
2020-06-12 12:34:20 +0300

    feat(guitar-looper-svg): fixed styling

--- projects/guitar-looper/src/lib/guitar-looper.component.ts
+++ projects/guitar-looper/src/lib/guitar-looper.component.ts
@@ -18,18 +18,19 @@ import { Component, OnInit } from '@angular/core';
           <stop stop-color="#767573" offset="100%"/>
         </radialGradient>
       </defs>
-      <g fill="none" fill-rule="evenodd">
-        <g transform="translate(0 179)">
+      <!-- INPUT and OUTPUT -->
+        <g transform="translate(0 179)" fill="url(#a)">
           <rect x="11" width="19" height="80" rx="9"/>
-          <rect fill="url(#a)" y="16" width="11" height="49" rx="3.5"/>
+          <rect y="16" width="11" height="49" rx="3.5"/>
         </g>
-        <g transform="translate(277 368)">
+        <g transform="translate(277 368)" fill="url(#a)">
           <rect width="19" height="80" rx="9"/>
-          <rect fill="url(#a)" x="19" y="16" width="11" height="49" rx="3.5"/>
+          <rect x="19" y="16" width="11" height="49" rx="3.5"/>
         </g>
+      <!-- Looper's body -->
         <g transform="translate(28)">
-          <rect stroke="#979797" fill="#2A2A2C" x=".5" y=".5" width="252" height="558" rx="42"/>
-          <ellipse stroke="#979797" fill="url(#b)" cx="125" cy="297.5" rx="14.5" ry="15"/>
+          <rect fill="#2A2A2C" x=".5" y=".5" width="252" height="558" rx="42"/>
+          <ellipse class="led" stroke="#979797" fill="url(#b)" cx="125" cy="297.5" rx="14.5" ry="15"/>
           <g transform="translate(73 44)">
             <ellipse stroke="#464648" fill="#121117" cx="54" cy="54.5" rx="53.5" ry="54"/>
             <rect stroke="#979797" fill="#FFF" x="52.5" y="3.5" width="5" height="41" rx="2.5"/>
@@ -39,20 +40,33 @@ import { Component, OnInit } from '@angular/core';
             <path fill="#BBB9B3" d="M105.75 28.57L134 77.5l-28.25 48.93h-56.5L21 77.5l28.25-48.93z"/>
             <circle fill="url(#c)" cx="77.5" cy="77.5" r="33.5"/>
           </g>
+          <g font-family="Noteworthy-Light,Noteworthy">
+            <text
+              font-size="16"
+              fill="#FFF"
+              transform="translate(110, 30)">
+              Level
+            </text>
+            <text font-size="16"
+                  fill="#FFF"
+                  transform="translate(85, 535)">
+              fk electronic
+            </text>
+            <text
+              font-size="16"
+              fill="#FFF"
+              transform="translate(20, 280)">
+              LOOPER
+            </text>
+            <text
+                  font-size="88"
+                  font-weight="300"
+                  fill="#FFF"
+                  transform="translate(20, 250)">
+              LITTO
+            </text>
+          </g>
         </g>
-        <text font-family="ShreeDev0714, Shree Devanagari 714" font-size="16" fill="#FFF" transform="translate(-14)">
-          <tspan x="147.72" y="31" font-family="LucidaGrande, Lucida Grande"> Level</tspan>
-        </text>
-        <text font-family="ShreeDev0714, Shree Devanagari 714" font-size="16" fill="#FFF" transform="translate(-14)">
-          <tspan x="60" y="544"> fk electronic</tspan>
-        </text>
-        <text font-family="ShreeDev0714, Shree Devanagari 714" font-size="16" fill="#FFF" transform="translate(-14)">
-          <tspan x="0" y="281"> LOOPER</tspan>
-        </text>
-        <text font-family="Noteworthy-Light, Noteworthy" font-size="16" font-weight="300" fill="#FFF" transform="translate(-14)">
-          <tspan x="63" y="157" font-size="88">LITTO</tspan>
-        </text>
-      </g>
     </svg>  `,
   styles: [
   ]
