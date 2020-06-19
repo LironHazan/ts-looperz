39506351ad907529a9d99faf63d49b40f6d99aba
lironh <lironenk@gmail.com>
2020-06-19 15:13:16 +0300

    fix(pedal): unified SVG

--- projects/guitar-looper/src/lib/guitar-looper.service.ts
+++ projects/guitar-looper/src/lib/guitar-looper.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class GuitarLooperService {
 
   constructor() { }
+
+  // potentiometer
+  clamp(min: number, max: number, value: number): number {
+    return Math.min(Math.max(value, min), max);
+  }
+
+  mapToMinMax(value: number, min: number, max: number): number {
+    return value * (max - min) + min;
+  }
+
+  percentFromMinMax(value: number, min: number, max: number): number {
+    return (value - min) / (max - min);
+  }
 }
--- projects/guitar-looper/src/lib/looper-pedal.component.ts
+++ projects/guitar-looper/src/lib/looper-pedal.component.ts
@@ -1,4 +1,5 @@
 import {Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {GuitarLooperService} from './guitar-looper.service';
 
 // !! some of the functions are taken from https://github.com/vitaliy-bobrov/js-rocks knob component
 // used https://github.com/wokwi/wokwi-elements potentiometer-element as main reference..
@@ -12,99 +13,7 @@ type LooperState = 'idle' | 'rec' | 'overlap' | 'stop' | 'cleared'; // todo: enu
 // Based on the Ditto looper of tc electronic (which I sold few months ago but I liked as a product..)
 @Component({
   selector: 'lib-looper-pedal',
-  template: `
-    <input type="range" min="1" max="200" value="100" class="slider" (input)="onZoomChanged($event)">
-    <span id="zoomValue">100%</span>
-    <input
-      #input
-      autofocus
-      tabindex="0"
-      type="range"
-      class="hide-input"
-      [max]="max"
-      [min]="min"
-      [value]="value"
-      [step]="step"
-      (change)="onValueChange($event)"/>
-    <svg [style.transform]="scale()"
-      id="zoom-pedal" width="307" height="559" xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <radialGradient cx="50%" cy="0%" fx="50%" fy="0%" r="445.455%" gradientTransform="matrix(0 .22449 -1 0 .5 -.112)" id="a">
-          <stop stop-color="#DCDCDC" offset="0%"/>
-          <stop stop-color="#0F0E0E" offset="100%"/>
-        </radialGradient>
-        <radialGradient cx="65.305%" cy="25.731%" fx="65.305%" fy="25.731%" r="103.333%"
-                        gradientTransform="matrix(0 .96774 -1.01393 0 .914 -.375)" id="led-filter">
-          <stop [attr.stop-color]="ledColor" offset="0%"/> <!-- Yellow #FBFF6E Red: E31111 Gray: #998B7F-->
-          <stop offset="100%"/>
-        </radialGradient>
-        <radialGradient cx="50%" cy="50%" fx="50%" fy="50%" r="100%" id="c">
-          <stop stop-color="#AEADAB" offset="0%"/>
-          <stop stop-color="#767573" offset="100%"/>
-        </radialGradient>
-        <radialGradient cx="50%" cy="50%" fx="50%" fy="50%" r="100%" id="stomp-btn-filter">
-          <stop stop-color="#AEADAB" offset="0%"/>
-          <stop stop-color="#767573" offset="100%"/>
-        </radialGradient>
-      </defs>
-      <!-- INPUT and OUTPUT -->
-      <g transform="translate(0 179)" fill="url(#a)">
-        <rect x="11" width="19" height="80" rx="9"/>
-        <rect y="16" width="11" height="49" rx="3.5"/>
-      </g>
-      <g transform="translate(277 368)" fill="url(#a)">
-        <rect width="19" height="80" rx="9"/>
-        <rect x="19" y="16" width="11" height="49" rx="3.5"/>
-      </g>
-      <!-- Looper's body -->
-      <g transform="translate(28)">
-        <rect fill="#2A2A2C" x=".5" y=".5" width="252" height="558" rx="42"/>
-      <!-- potentiometer -->
-      <g  transform="translate(73 44)"
-          (click)="onFocus()"
-          (mousedown)="down($event)"
-          (mouseup)="up()"
-          (mousemove)="move($event)">
-          <ellipse class="knob" #knob cx="54" cy="54" rx="53" ry="54"/>
-          <rect [style.--knob-angle]="calcKnobDeg()" id="rotating"  x="52" y="3" width="5" height="41" rx="2"/>
-      </g>
-        <!-- LED -->
-        <ellipse class="led" stroke="#979797" fill="url(#led-filter)" cx="125" cy="297.5" rx="14.5" ry="15"/>
-        <!-- stomp btn -->
-        <g class="stomp-btn">
-          <circle fill="#AEADAB" opacity=".811" cx="77.5" cy="78.5" r="57.5"/>
-          <path d="M105.461 29.07H49.54L21.577 77.5l27.962 48.43h55.922l27.962-48.43-27.962-48.43z" stroke="#969292" fill="#A3A098"/>
-          <circle (click)="changeLoopState()" class="push-area" stroke="#7F7C7C" fill="url(#stomp-btn-filter)" cx="76.5" cy="78.5" r="32"/>
-        </g>
-        <!-- labels -->
-        <g font-family="Noteworthy-Light,Noteworthy">
-          <text
-            font-size="16"
-            fill="#FFF"
-            transform="translate(110, 30)">
-            Level
-          </text>
-          <text font-size="16"
-                fill="#FFF"
-                transform="translate(85, 535)">
-            fk electronic
-          </text>
-          <text
-            font-size="16"
-            fill="#FFF"
-            transform="translate(20, 280)">
-            LOOPER
-          </text>
-          <text
-            font-size="88"
-            font-weight="300"
-            fill="#FFF"
-            transform="translate(20, 250)">
-            LITTO
-          </text>
-        </g>
-      </g>
-    </svg> `,
+  templateUrl: './looper-pedal.component.html',
   styleUrls: ['./looper-pedal.component.css']
 })
 export class LooperPedalComponent implements OnInit {
@@ -133,7 +42,7 @@ export class LooperPedalComponent implements OnInit {
 
   @Output() vol: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor(private looperService: GuitarLooperService) { }
 
   ngOnInit(): void {
   }
@@ -144,21 +53,9 @@ export class LooperPedalComponent implements OnInit {
   }
 
   calcKnobDeg() {
-    const percent = this.clamp(0, 1, this.percentFromMinMax(this.value, this.min, this.max));
+    const percent = this.looperService.clamp(0, 1, this.looperService.percentFromMinMax(this.value, this.min, this.max));
     const deg = (this.endDegree - this.startDegree) * percent + this.startDegree;
     return `${deg}deg`;
-  }
-
-  clamp(min: number, max: number, value: number): number {
-    return Math.min(Math.max(value, min), max);
-  }
-
-  mapToMinMax(value: number, min: number, max: number): number {
-    return value * (max - min) + min;
-  }
-
-  percentFromMinMax(value: number, min: number, max: number): number {
-    return (value - min) / (max - min);
   }
 
   onValueChange({target}) {
@@ -218,15 +115,15 @@ export class LooperPedalComponent implements OnInit {
       deg -= 360;
     }
 
-    deg = this.clamp(this.startDegree, this.endDegree, deg);
-    const percent = this.percentFromMinMax(deg, this.startDegree, this.endDegree);
-    const value = this.mapToMinMax(percent, this.min, this.max);
+    deg = this.looperService.clamp(this.startDegree, this.endDegree, deg);
+    const percent = this.looperService.percentFromMinMax(deg, this.startDegree, this.endDegree);
+    const value = this.looperService.mapToMinMax(percent, this.min, this.max);
 
     this.updateValue(value);
   }
 
   private updateValue(value: number) {
-    const clamped = this.clamp(this.min, this.max, value);
+    const clamped = this.looperService.clamp(this.min, this.max, value);
     const updated = Math.round(clamped / this.step) * this.step;
     this.value = Math.round(updated * 100) / 100;
     this.vol.emit(this.value);
